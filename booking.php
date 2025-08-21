5292e03db98d46a56daaeb7acd9b7e05a41b1bb9
suresh C <sureshs68167@gmail.com>
2025-08-21 22:23:01 +0530

    updated

--- booking.php
+++ booking.php
@@ -12,8 +12,9 @@
         href="https://fonts.googleapis.com/css2?family=League+Gothic&family=Oswald:wght@200..700&family=Outfit:wght@100..900&family=Rubik+Wet+Paint&family=Varela&display=swap"
         rel="stylesheet">
     <link rel="shortcut icon" href="./assets/fav-icon.png" type="image/x-icon">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./booking.css">
 </head>
 
 <body>
